de14fb2c2d80e124c5f03dc3d83b1c044684e665
PatrykOlechno <patrykoolechno@gmail.com>
2020-10-08 20:40:22 +0200

    Added search bar

--- github_info.js
+++ github_info.js
@@ -1,23 +1,42 @@
-const basic = document.querySelector('.basic-info');
+const img = document.querySelector('img');
 const advanced_repos = document.querySelector('.repos');
 const nick = document.querySelector('.nick');
+const link = document.querySelector('a');
+let login = 'PatrykOlechno';
+//search setup
+const input = document.querySelector('input');
+
+input.addEventListener("keyup", event => {
+    if (event.keyCode === 13) {
+        login = input.value
+        getGithub();
+    }
+});
 
 async function getGithub() {
-    const res = await fetch('https://api.github.com/users/patrykolechno');
+    const res = await fetch(`https://api.github.com/users/${login}`);
 
     if (!res.ok){
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        alert("Login doesn't match any user!");
+        throw new Error(`HTTP error! Status: ${res.status}`);
     }else{
+        //setup
         const json =  await res.json();
         const avatar = new Image();
-        avatar.src = json.avatar_url;
-        const login = document.createTextNode(json.login);
+        img.src = json.avatar_url;
+        const login = json.login;
         const html_url = json.html_url;
-        const repos = document.createTextNode(json.public_repos)
+        const repos = json.public_repos;
 
-        basic.appendChild(avatar);
-        nick.appendChild(login)
-        advanced_repos.appendChild(repos);
+        //avatar
+        link.href = html_url;
+        link.target = "_blank";
+        
+        //nick
+        login == "[object Text]" ? nick.value = '' : nick.value = login;
+
+        //repos
+        advanced_repos.innerHTML = repos;
     }
 }
 
